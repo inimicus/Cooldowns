3bdbb314a04175632691544b84585896d8fcffab
Garret Jennings <gjennings@me.com>
2018-10-14 11:26:18 -0700

    Move Tracking functions from globals to locals.

--- src/Tracking.lua
+++ src/Tracking.lua
@@ -11,11 +11,87 @@ Cool.Tracking = {}
 local updateIntervalMs = 100
 
 -- ----------------------------------------------------------------------------
+-- Callback Functions
+-- ----------------------------------------------------------------------------
+
+local function OnCooldownUpdated(setKey, eventCode, abilityId)
+    -- When cooldown of this ability occurs, this function is continually called
+    -- until the set is off cooldown.
+    -- We can use the first call of this function to detect a proc state.
+
+    local set = Cool.Data.Sets[setKey]
+
+    -- Ignore if set is on cooldown
+    if set.onCooldown == true then return end
+
+    Cool:Trace(1, zo_strformat("Cooldown proc for <<1>> (<<2>>)", setKey, abilityId))
+
+    set.onCooldown = true
+    set.timeOfProc = GetGameTimeMilliseconds()
+    Cool.UI.PlaySound(Cool.preferences.sets[setKey].sounds.onProc.sound)
+    EVENT_MANAGER:RegisterForUpdate(Cool.name .. setKey .. "Count", updateIntervalMs, function(...) Cool.UI.Update(setKey) return end)
+end
+
+local function OnCombatEvent(setKey, _, result, _, abilityName, _, _, _, _, _, _, _, _, _, _, _, _, abilityId)
+
+    local set = Cool.Data.Sets[setKey]
+
+    if result == ACTION_RESULT_ABILITY_ON_COOLDOWN then
+        Cool:Trace(1, zo_strformat("<<1>> (<<2>>) on Cooldown", abilityName, abilityId))
+    elseif result == set.result then
+        Cool:Trace(1, zo_strformat("Name: <<1>> ID: <<2>> with result <<3>>", abilityName, abilityId, result))
+        set.onCooldown = true
+        set.timeOfProc = GetGameTimeMilliseconds()
+        Cool.UI.PlaySound(Cool.preferences.sets[setKey].sounds.onProc.sound)
+        EVENT_MANAGER:RegisterForUpdate(Cool.name .. setKey .. "Count", updateIntervalMs, function(...) Cool.UI.Update(setKey) return end)
+    else
+        Cool:Trace(1, zo_strformat("Name: <<1>> ID: <<2>> with result <<3>>", abilityName, abilityId, result))
+    end
+
+end
+
+local function IsInCombat(_, inCombat)
+    Cool.isInCombat = inCombat
+    Cool:Trace(2, zo_strformat("In Combat: <<1>>", tostring(inCombat)))
+    Cool.UI:SetCombatStateDisplay()
+end
+
+local function OnAlive()
+    Cool.HUDHidden = false
+    Cool.UI:SetCombatStateDisplay()
+end
+
+local function OnDeath()
+    Cool.HUDHidden = true
+    Cool.UI:SetCombatStateDisplay()
+end
+
+local function OnCombatEventUnfiltered(_, result, _, abilityName, _, _, _, _, _, _, _, _, _, _, _, _, abilityId)
+    -- Exclude common unnecessary abilities
+    local ignoreList = {
+        sprint        = 973,
+        sprintDrain   = 15356,
+        block         = 14890,
+        interrupt     = 55146,
+        roll          = 28549,
+        immov         = 29721,
+        phase         = 98294,
+        dodgeFatigue  = 69143,
+    }
+
+    for index, value in pairs(ignoreList) do
+        if abilityId == value then return end
+    end
+
+    Cool:Trace(1, zo_strformat("<<1>> (<<2>>) with result <<3>>", abilityName, abilityId, result))
+end
+
+-- ----------------------------------------------------------------------------
 -- Event Register/Unregister
 -- ----------------------------------------------------------------------------
 
 function Cool.Tracking.RegisterUnfiltered()
-    EVENT_MANAGER:RegisterForEvent(Cool.name .. "_Unfiltered", EVENT_COMBAT_EVENT, Cool.Tracking.OnCombatEventUnfiltered)
+    EVENT_MANAGER:RegisterForEvent(Cool.name .. "_Unfiltered", EVENT_COMBAT_EVENT, OnCombatEventUnfiltered)
     EVENT_MANAGER:AddFilterForEvent(Cool.name .. "_Unfiltered", EVENT_COMBAT_EVENT, REGISTER_FILTER_SOURCE_COMBAT_UNIT_TYPE, COMBAT_UNIT_TYPE_PLAYER)
     Cool:Trace(1, "Registered Unfiltered Events")
 end
@@ -26,8 +102,8 @@ function Cool.Tracking.UnregisterUnfiltered()
 end
 
 function Cool.Tracking.RegisterEvents()
-    EVENT_MANAGER:RegisterForEvent(Cool.name, EVENT_PLAYER_ALIVE, Cool.Tracking.OnAlive)
-    EVENT_MANAGER:RegisterForEvent(Cool.name, EVENT_PLAYER_DEAD, Cool.Tracking.OnDeath)
+    EVENT_MANAGER:RegisterForEvent(Cool.name, EVENT_PLAYER_ALIVE, OnAlive)
+    EVENT_MANAGER:RegisterForEvent(Cool.name, EVENT_PLAYER_DEAD, OnDeath)
     EVENT_MANAGER:RegisterForEvent(Cool.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE, Cool.Equipped.WornSlotUpdate)
     EVENT_MANAGER:AddFilterForEvent(Cool.name, EVENT_INVENTORY_SINGLE_SLOT_UPDATE,
         REGISTER_FILTER_BAG_ID, BAG_WORN,
@@ -48,7 +124,7 @@ function Cool.Tracking.UnregisterEvents()
 end
 
 function Cool.Tracking.RegisterCombatEvent()
-    EVENT_MANAGER:RegisterForEvent(Cool.name .. "COMBAT", EVENT_PLAYER_COMBAT_STATE, function(...) Cool.IsInCombat(...) end)
+    EVENT_MANAGER:RegisterForEvent(Cool.name .. "COMBAT", EVENT_PLAYER_COMBAT_STATE, IsInCombat)
     Cool:Trace(2, "Registered combat events")
 end
 
@@ -74,9 +150,9 @@ function Cool.Tracking.EnableTrackingForSet(setKey, enabled)
             -- Set callback based on event
             local procFunction = nil
             if set.event == EVENT_ABILITY_COOLDOWN_UPDATED then
-                procFunction = Cool.Tracking.OnCooldownUpdated
+                procFunction = OnCooldownUpdated
             else
-                procFunction = Cool.Tracking.OnCombatEvent
+                procFunction = OnCombatEvent
             end
 
             -- Register events
@@ -105,81 +181,5 @@ function Cool.Tracking.EnableTrackingForSet(setKey, enabled)
         end
     end
 
-end
-
--- ----------------------------------------------------------------------------
--- Callback Functions
--- ----------------------------------------------------------------------------
-
-function Cool.Tracking.OnCooldownUpdated(setKey, eventCode, abilityId)
-    -- When cooldown of this ability occurs, this function is continually called
-    -- until the set is off cooldown.
-    -- We can use the first call of this function to detect a proc state.
-
-    local set = Cool.Data.Sets[setKey]
-
-    -- Ignore if set is on cooldown
-    if set.onCooldown == true then return end
-
-    Cool:Trace(1, zo_strformat("Cooldown proc for <<1>> (<<2>>)", setKey, abilityId))
-
-    set.onCooldown = true
-    set.timeOfProc = GetGameTimeMilliseconds()
-    Cool.UI.PlaySound(Cool.preferences.sets[setKey].sounds.onProc.sound)
-    EVENT_MANAGER:RegisterForUpdate(Cool.name .. setKey .. "Count", updateIntervalMs, function(...) Cool.UI.Update(setKey) return end)
-end
-
-function Cool.Tracking.OnCombatEvent(setKey, _, result, _, abilityName, _, _, _, _, _, _, _, _, _, _, _, _, abilityId)
-
-    local set = Cool.Data.Sets[setKey]
-
-    if result == ACTION_RESULT_ABILITY_ON_COOLDOWN then
-        Cool:Trace(1, zo_strformat("<<1>> (<<2>>) on Cooldown", abilityName, abilityId))
-    elseif result == set.result then
-        Cool:Trace(1, zo_strformat("Name: <<1>> ID: <<2>> with result <<3>>", abilityName, abilityId, result))
-        set.onCooldown = true
-        set.timeOfProc = GetGameTimeMilliseconds()
-        Cool.UI.PlaySound(Cool.preferences.sets[setKey].sounds.onProc.sound)
-        EVENT_MANAGER:RegisterForUpdate(Cool.name .. setKey .. "Count", updateIntervalMs, function(...) Cool.UI.Update(setKey) return end)
-    else
-        Cool:Trace(1, zo_strformat("Name: <<1>> ID: <<2>> with result <<3>>", abilityName, abilityId, result))
-    end
-
-end
-
-function Cool.IsInCombat(_, inCombat)
-    Cool.isInCombat = inCombat
-    Cool:Trace(2, zo_strformat("In Combat: <<1>>", tostring(inCombat)))
-    Cool.UI:SetCombatStateDisplay()
-end
-
-function Cool.Tracking.OnAlive()
-    Cool.HUDHidden = false
-    Cool.UI:SetCombatStateDisplay()
-end
-
-function Cool.Tracking.OnDeath()
-    Cool.HUDHidden = true
-    Cool.UI:SetCombatStateDisplay()
-end
-
-function Cool.Tracking.OnCombatEventUnfiltered(_, result, _, abilityName, _, _, _, _, _, _, _, _, _, _, _, _, abilityId)
-    -- Exclude common unnecessary abilities
-    local ignoreList = {
-        sprint        = 973,
-        sprintDrain   = 15356,
-        block         = 14890,
-        interrupt     = 55146,
-        roll          = 28549,
-        immov         = 29721,
-        phase         = 98294,
-        dodgeFatigue  = 69143,
-    }
-
-    for index, value in pairs(ignoreList) do
-        if abilityId == value then return end
-    end
-
-    Cool:Trace(1, zo_strformat("<<1>> (<<2>>) with result <<3>>", abilityName, abilityId, result))
 end
 

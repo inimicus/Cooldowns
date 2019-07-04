a5bcd03527155672e263ccd7f8fa37ca153a6b26
Garret Jennings <gjennings@me.com>
2019-07-03 22:46:32 -0700

    Bump to version 1.6.0

--- src/Main.lua
+++ src/Main.lua
@@ -9,7 +9,7 @@
 -- -----------------------------------------------------------------------------
 Cool            = {}
 Cool.name       = "Cooldowns"
-Cool.version    = "1.5.3"
+Cool.version    = "1.6.0"
 Cool.dbVersion  = 1
 Cool.slash      = "/cool"
 Cool.prefix     = "[Cooldowns] "

4cc6295ad4f2266c83eb6b84a90c4294aceb1fe4
Garret Jennings <gjennings@me.com>
2019-09-02 13:50:55 -0700

    Bumped to version 1.7.0
    Automatically committed via package v1.0.0-beta.3

--- src/Main.lua
+++ src/Main.lua
@@ -9,7 +9,7 @@
 -- -----------------------------------------------------------------------------
 Cool            = {}
 Cool.name       = "Cooldowns"
-Cool.version    = "1.6.1"
+Cool.version    = "1.7.0"
 Cool.dbVersion  = 1
 Cool.slash      = "/cool"
 Cool.prefix     = "[Cooldowns] "
